e062b5b7f80f0dd50b7df2bd9bc6abb466bbe17a
Oleksandr <153434949+Sashobrine@users.noreply.github.com>
2023-12-14 01:51:46 +0200

    create new file

---
+++ task1.py
@@ -0,0 +1,6 @@
+number1 = int(input("Введіть число  1: "))
+number2 = int(input("Введіть число  2: "))
+
+print(f"{number1}+{number2}={number1 + number2}")
+print(f"{number1}-{number2}={number1 - number2}")
+print(f"{number1}:{number2}={number1 // number2}")
