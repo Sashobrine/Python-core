1d761985052d4d881d317d5f5c68f0d338984591
Oleksandr <153434949+Sashobrine@users.noreply.github.com>
2023-12-14 03:05:18 +0200

    complite tasks

---
+++ homework/task1.py
@@ -0,0 +1,6 @@
+number1 = int(input("Введіть число  1: "))
+number2 = int(input("Введіть число  2: "))
+number3 = int(input("Введіть число  3: "))
+
+print(f"{number1}+{number2}+{number3}={number1 + number2 + number3}")
+print(f"{number1}*{number2}*{number3}={number1 * number2 * number3}")
---
+++ homework/task2.py
@@ -0,0 +1,5 @@
+salery = int(input("enter summ: "))
+bank_percent = int(input("enter summ: "))
+payments = int(input("enter summ: "))
+
+print(f"will remain: {salery - bank_percent - payments}UAH")
