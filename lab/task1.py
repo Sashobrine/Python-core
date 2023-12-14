c54c82326d5817de51ad04e3a5a5bd95a153b86e
Oleksandr <153434949+Sashobrine@users.noreply.github.com>
2023-12-14 03:11:41 +0200

    fix calc formula

--- lab/task1.py
+++ lab/task1.py
@@ -3,4 +3,4 @@ number2 = int(input("Введіть число  2: "))
 
 print(f"{number1}+{number2}={number1 + number2}")
 print(f"{number1}-{number2}={number1 - number2}")
-print(f"{number1}:{number2}={number1 // number2}")
+print(f"{number1}*{number2}={number1 * number2}")
