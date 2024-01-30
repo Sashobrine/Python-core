c4f6dbb34d966011e2194f34f690fa183dc40184
Oleksandr <153434949+Sashobrine@users.noreply.github.com>
2024-01-30 03:19:43 +0200

    add new code in task

--- main/module4/lab/task2.py
+++ main/module4/lab/task2.py
@@ -6,3 +6,15 @@ for i in range(1, number + 1):
     factorial *= i
 
 print(f"факторіал числа {number} буде: {factorial}")
+
+a = int(input("enter number : "))
+
+
+def factorial(a):
+    result = 1
+    for i in range(1, a + 1):
+        result *= i
+    return result
+
+
+print(f"факторіал числа {a} буде:  {factorial(a)}")
